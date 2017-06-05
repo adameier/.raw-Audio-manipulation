f156bd508c1af94f9e6a23b150ad34406fbd8b7f
adam meier <adam.meier2@gmail.com>
2017-06-05 22:44:18 +0200

    Initial commit. Created audio.h file to hold templating for Audio class.

---
+++ audio.h
@@ -0,0 +1,15 @@
+#ifndef AUDIO_H
+#define AUDIO_H
+
+#include <vector>
+#include <cstdint>
+
+namespace MRXADA002 {
+    template<typename T> class Audio {
+    private:
+        std::vector<T> data_vector;
+    public:
+    };
+}
+
+#ifndef
